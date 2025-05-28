8b11e3bd03cc111d4826457c61b63c1fe9b4d71a
Flooreyes <wyatt@aori.io>
2025-05-28 12:44:00 -0400

    addresses

--- src/constants.rs
+++ src/constants.rs
@@ -12,28 +12,28 @@ pub fn get_chains_map() -> HashMap<String, ChainInfo> {
         chain_key: "ethereum".to_string(),
         chain_id: 1,
         eid: 30101,
-        address: "0xeA5c82C81CCc0ba69616c6eae40A6EC7F7794d87".to_string(),
+        address: "0xAC23dd76E55E15be6bB10057c37fCF307cd0bfD5".to_string(),
     });
     
     chains.insert("base".to_string(), ChainInfo {
         chain_key: "base".to_string(),
         chain_id: 8453,
         eid: 30184,
-        address: "0xBF693fcE30E7B08965E10A7ECddc92818d6a2a1e".to_string(),
+        address: "0xf0304563e05B1E2Bc3De8DC80185E8Ca2940CA04".to_string(),
     });
     
     chains.insert("arbitrum".to_string(), ChainInfo {
         chain_key: "arbitrum".to_string(),
         chain_id: 42161,
         eid: 30110,
-        address: "0x437266584AdEae66F0edF0B97d14F399C8463731".to_string(),
+        address: "0x83dE87A541613B50263A216a210B8fcdfd5DBc2C".to_string(),
     });
     
     chains.insert("optimism".to_string(), ChainInfo {
         chain_key: "optimism".to_string(),
         chain_id: 10,
         eid: 30111,
-        address: "0xAA8Ec1a2C2814aAc925107e2b3c94ee0E8367ab5".to_string(),
+        address: "0x62438859025E2DDd589F21c109de4C77EE308C91".to_string(),
     });
     
     chains
